5d7a9109dacf518cf1edb8909ce509019226abfc
hehai <979159359@qq.com>
2018-09-09 22:40:09 +0800

    spring-cloud

---
+++ microservice-simple-consumer-movie/src/main/java/com/uzengroup/controller/MovieController.java
@@ -0,0 +1,21 @@
+package com.uzengroup.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import com.uzengroup.model.User;
+
+@RestController
+public class MovieController {
+
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@GetMapping("/{id}")
+	public User findById(@PathVariable Long id) {
+		User findOne = this.restTemplate.getForObject("http://localhost:8000/"+id, User.class);
+	    return findOne;
+	}
+}
